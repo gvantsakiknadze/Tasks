2cbb03c023aae1b46cb4f1d3fdc7292638896f91
Gvantsa Kiknadze <115503418+gvantsakiknadze@users.noreply.github.com>
2023-02-20 15:24:50 +0400

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,5 +1,23 @@
 def isProperly(sequence):
     counter = 0
+    mapping = {
+        "(" : 1,
+        ")" : -1,
+    }
+    
+    
+    conter = 0
+    for i in sequence:
+        counter += mapping[i]
+        if counter < 0:
+            return False
+    return True
+    
+
+
+
+def isProperly2(sequence):
+    counter = 0
     for i in sequence:
         if i == "(":
             counter += 1
