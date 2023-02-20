bf39bcd739ef6b64b5e3ef9351b74a9670be24ca
Gvantsa Kiknadze <115503418+gvantsakiknadze@users.noreply.github.com>
2023-02-20 15:20:22 +0400

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,3 +1,12 @@
+
+def sumOfDigits(start,end):
+    sum_of_digits = 0 
+    for number in range(start, end+1):
+        sum_of_digits += sum([int(digit) for digit in str(number)])
+    return sum_of_digits
+
+
+
 def sum(i):
     sum = 0
     while i != 0:
@@ -6,11 +15,9 @@ def sum(i):
     return sum
 
 
-def sumOfDigits(x,y):
+def sumOfDigits2(x,y):
     final_sum = 0
     for i in range(x,y+1):
         final_sum += sum(i)
     return final_sum
 
-
-print(sumOfDigits(17, 20))
